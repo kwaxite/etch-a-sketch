9cbfc6bf856799cfb9779ae828a7a0e834aaf519
kwaxite <146014461+kwaxite@users.noreply.github.com>
2023-12-21 09:44:32 +0000

    Add random color option

--- gridtoy.js
+++ gridtoy.js
@@ -17,11 +17,14 @@ const changeColor = (el) => {
 }
 
 // function changes background colour to white
-const removeColor = (el) => {
-    el.target.style.backgroundColor = generateRandomColor()
+const whiteColor = (el) => {
+    el.target.style.backgroundColor = 'white'
 }
 
-
+// function changes background colour to white
+const randomColor = (el) => {
+    el.target.style.backgroundColor = generateRandomColor()
+}
 
 // calculate dimensions of each square 
 let numOfSquares = 1
@@ -29,7 +32,7 @@ const gridSize = 480
 let squareSize = gridSize/numOfSquares
 
 // function to create square boxes
-const createBox = (container, newDiv) => {
+const createBox = (container, newDiv, colorType) => {
     newDiv = document.createElement('div')
     newDiv.setAttribute('class', 'box')
     newDiv.style.outline = "0.5px solid purple"
@@ -37,21 +40,30 @@ const createBox = (container, newDiv) => {
     newDiv.style.width =  `${squareSize}px`
     // newDiv.style.margin = "10px"
     newDiv.addEventListener("mouseover", changeColor)
-    newDiv.addEventListener("mouseout", removeColor)
+    newDiv.addEventListener("mouseout", colorType)
     container.append(newDiv)
 }
 
 
-// function to create n number of boxes
+// function to create n number of grid - reveal white color
 
-const createBoxes = (container, num) => {
+const whiteColorFill = (container, num) => {
     for (let i = 0; i < num; i++) {
         for (let i = 0; i < num; i++){
-            createBox(container, i)
+            createBox(container, i, whiteColor)
         }
     }
 }
 
+// function to create n number of grid - random white color
+
+const randomColorFill = (container, num) => {
+    for (let i = 0; i < num; i++) {
+        for (let i = 0; i < num; i++){
+            createBox(container, i, randomColor)
+        }
+    }
+}
 
 
 // function to remove boxes
@@ -75,36 +87,57 @@ header.style.margin = "30px"
 const nav = document.querySelector('nav')
 console.log(nav)
 
-const displayButton = document.createElement('button')
-displayButton.setAttribute('class', 'btn display')
+const whiteColorButton = document.createElement('button')
+whiteColorButton.setAttribute('class', 'btn display')
+
+const randomColorButton = document.createElement('button')
+whiteColorButton.setAttribute('class', 'btn display')
+
 const resetButton = document.createElement('button')
 resetButton.setAttribute('class', 'btn reset')
 
-nav.append(displayButton)
+nav.append(whiteColorButton)
+
+nav.append(randomColorButton)
+
 nav.append(resetButton)
 
 
 console.log(resetButton)
 console.log(nav)
 
-displayButton.textContent = 'Number of squares'
-displayButton.style.cssText = "font-size: 1rem; background-color:yellow"
-displayButton.style.margin = "10px"
-displayButton.style.padding = "5px"
+whiteColorButton.textContent = 'White'
+whiteColorButton.style.cssText = "font-size: 1rem; background-color:yellow"
+whiteColorButton.style.margin = "10px"
+whiteColorButton.style.padding = "5px"
+
+randomColorButton.textContent = 'Random'
+randomColorButton.style.cssText = "font-size: 1rem; background-color:pink"
+randomColorButton.style.margin = "10px"
+randomColorButton.style.padding = "5px"
 
 resetButton.textContent = 'Reset'
 resetButton.style.cssText = "font-size: 1rem; background-color:lightgreen"
 resetButton.style.margin = "10px"
 resetButton.style.padding = "5px"
 
-// prompt asking for the number of squares
-const displayGrid = () => {
+// reveal white - prompt asking for the number of squares
+const displayGridWhite = () => {
     numOfSquares = prompt("Enter number of squares from 1 and 100");
     squareSize = gridSize/numOfSquares
-    createBoxes(container, numOfSquares)
+    whiteColorFill(container, numOfSquares)
 }
 
-displayButton.addEventListener('click',displayGrid)
+whiteColorButton.addEventListener('click',displayGridWhite)
+
+// reveal random color - prompt asking for the number of squares
+const displayGridColor = () => {
+    numOfSquares = prompt("Enter number of squares from 1 and 100");
+    squareSize = gridSize/numOfSquares
+    randomColorFill(container, numOfSquares)
+}
+
+randomColorButton.addEventListener('click',displayGridColor)
 
 // reset grid
 
